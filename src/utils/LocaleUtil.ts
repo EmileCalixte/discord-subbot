9edc2e326d74fb289c61cee0a48c02725965482c
Emile Calixte <calixte.emile@gmail.com>
2022-12-12 21:23:08 +0100

    Create LocaleUtil

---
+++ src/utils/LocaleUtil.ts
@@ -0,0 +1,22 @@
+import {LocaleString, LocalizationMap} from "discord.js";
+
+type DefaultLocale = "en-US";
+
+/**
+ * A {@see LocalizationMap} object with a default locale
+ */
+export type LocalizationMapWithDefault = LocalizationMap & {
+    [key in DefaultLocale]: string;
+}
+
+export function getLocaleString(locale: LocaleString, localizationMap: LocalizationMapWithDefault): string {
+    if (!(locale in localizationMap)) {
+        return localizationMap["en-US"];
+    }
+
+    if (localizationMap[locale] === null) {
+        return localizationMap["en-US"];
+    }
+
+    return localizationMap[locale];
+}
